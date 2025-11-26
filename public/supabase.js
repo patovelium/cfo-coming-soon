5d4612f6c010f7d1fa6423d0d851601733d1b85d
patovelium <schietekat.p@gmail.com>
2025-11-25 18:19:32 -0600

    Update supabase.js

--- public/supabase.js
+++ public/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = https://vvgspijjrvjofomsgiwi.supabase.co
-const supabaseAnonKey = sb_publishable_ucWhyBuE3wKSUr-kqdUbog_BYaMAE7q
+const supabaseUrl = "https://vvgspijjrvjofomsgiwi.supabase.co"
+const supabaseAnonKey = "sb_publishable_ucWhyBuE3wKSUr-kqdUbog_BYaMAE7q"
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
